5b74ce32086e7739273268088aa24c0c09966886
helonayala <helon@puc-rio.br>
2018-09-05 03:00:07 +0000

    FR pred and graphs

--- armax.R
+++ armax.R
@@ -9,6 +9,7 @@ cat("\014")
 library(ggplot2) # fancy plots
 library(signal)  # filter for input signal
 library(MASS)
+library(tidyr)
 
 # load functions
 source("library_sysid.R")
@@ -20,10 +21,10 @@ N = 1000 # number of samples
 na = 4
 nb = 3
 p  = 1 + max(na,nb)
-sd_noise = 1e-2
+sd_noise = 1e-1
 # create input signal
 bf = butter(6, 1/10, type="low")
-u  = filter(bf, rnorm(N,mean=0,sd=1))
+u  = signal::filter(bf, rnorm(N,mean=0,sd=1))
 
 plot(u)
 
@@ -40,17 +41,51 @@ Y   = targetVec(y,na,nb)
 
 th_hat = ginv(Phi) %*% Y
 
+# calculate predictions
+y_osa = (Phi %*% th_hat)[,]
+y_fr = y[1:(p-1)]
+u_fr = u[1:(p-1)]
+for (k in p:N){
+  phi_k = regMatrix(c(y_fr[(k-p+1):(k-1)],0),c(u_fr[(k-p+1):(k-1)],0),na,nb)
+  y_fr[k] = phi_k %*% th_hat
+  u_fr[k] = u[k]
+}
+y_fr = y_fr[p:N]
+
 df_dataset = tibble(time = 1:N,
                         y = y,
                         yor = yor,
                         u = u) %>%
   gather(variable, measurement, -time)
 
-ggplot(data=filter(df_dataset,variable %in% c("y","yor"))) + 
-  geom_line(aes(x = time,y =measurement,color=variable))
+df_pred = tibble(time = p:N,
+                 y_osa = y_osa,
+                 y_fr = y_fr) %>%
+  gather(variable, measurement, -time)
 
-ggplot(data=filter(df_dataset,variable %in% c("u"))) + 
-  geom_line(aes(x = time,y =measurement,color=variable))
+df_error = tibble(time = p:N,
+                 e_osa = Y - y_osa,
+                 e_fr = Y - y_fr) %>%
+  gather(variable, measurement, -time)
+
+
+df_all = bind_rows(df_dataset,df_pred)
+
+# ggplot(data=filter(df_dataset,variable %in% c("y","yor"))) + 
+#   geom_line(aes(x = time,y =measurement,color=variable)) + 
+#   ggtitle("Output")
+
+ggplot(data=filter(df_all,variable %in% c("yor","y_osa","y_fr"))) + 
+  geom_line(aes(x = time,y =measurement,color=variable)) + 
+  ggtitle("predictions")
+
+ggplot(data=filter(df_error,variable %in% c("e_osa","e_fr"))) + 
+  geom_line(aes(x = time,y =measurement,color=variable)) + 
+  ggtitle("prediction residuals")
+
+# ggplot(data=filter(df_dataset,variable %in% c("u"))) + 
+#   geom_line(aes(x = time,y =measurement,color=variable)) + 
+#   ggtitle("Input")
 
 
 
