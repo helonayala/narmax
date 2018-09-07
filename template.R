7898211154afed4598f4bc5c63e919154a92c7b6
helonayala <helon@puc-rio.br>
2018-09-07 14:02:40 +0000

    template for code

--- template.R
+++ template.R
@@ -2,10 +2,8 @@
 # helon - 4/9/18
 # mec2015 - system identification - puc-rio
 
-
-
 rm(list=ls()) # remove all vars and functions
 cat("\014")   # clean console
 while(!is.null(dev.list())) dev.off()     # clear all graphs
 
-
+# code begins
