a8c8814701a0185fa683dcb8e1dd2a7f2b362f10
helon <helon@puc-rio.br>
2018-09-08 10:41:10 -0300

    finished regression matrix. next: FROLS

--- example_3_6_FROLS.R
+++ example_3_6_FROLS.R
@@ -6,8 +6,9 @@ rm(list=ls()) # remove all vars and functions
 cat("\014")   # clean console
 while(!is.null(dev.list())) dev.off()     # clear all graphs
 
-# code begins
+source("library_sysid.R")
 
+# code begins
 
 # generate simulation data ------------------------------------------------
 N = 200
@@ -18,30 +19,17 @@ for (k in 3:N) {
   y[k] = -0.605*y[k-1] - 0.163*y[k-2]^2 + 0.588*u[k-1] - 0.24*u[k-2] + e[k]
 }
 
-
 # model parameters --------------------------------------------------------
 nu = 2
-ny = 2
+ny = 3
 ne = 0
 l = 3
 n = nu + ny + ne
+p = max(nu,ny,nu)+1
 # candidate
 ncan = 0
 canl = NULL
 
-Phi = regMatNARX(nu,ny,l)
+Phi = regMatNARX(u,y,nu,ny,l)
+Y = targetVec(y,p)
 
-canl3 = expand.grid(1:n,1:n,1:n)
-canl2 = expand.grid(1:n,1:n)
-canl1 = expand.grid(1:n)
-
-canl3_a = t(apply(canl3,1,sort)) # order each row
-canl3_b = unique(canl3_a) # keep unique values
-canl2_a = t(apply(canl2,1,sort)) # order each row
-canl2_b = unique(canl2_a) # keep unique values
-canl1_a = t(apply(canl1,1,sort)) # order each row
-canl1_b = unique(canl1_a) # keep unique values
-
-canl1_b
-canl2_b
-canl3_b
