58bfa395dcc8483203f967485f7d77f78dfb36d2
helonayala <helon@puc-rio.br>
2018-09-13 15:03:40 -0300

    finished example 3.6 (results OK with, checked with 0 noise)

--- example_3_6_FROLS.R
+++ example_3_6_FROLS.R
@@ -8,6 +8,7 @@ while(!is.null(dev.list())) dev.off()     # clear all graphs
 
 library(tidyverse)
 source("library_sysid.R")
+set.seed(0) # reproducibility
 
 # code begins
 
@@ -21,14 +22,90 @@ for (k in 3:N) {
 }
 
 # model parameters --------------------------------------------------------
+rho = 0.04
 nu = 2
-ny = 3
+ny = 2
 ne = 0
 l = 3
 n = nu + ny + ne
 p = max(nu,ny,nu)+1
 
 # create regression and target matrices -----------------------------------
-Phi = regMatNARX(u,y,nu,ny,l)
+P = regMatNARX(u,y,nu,ny,l)
 Y = targetVec(y,p)
+
+M = ncol(P)
+NP = nrow(P)
+
+# 1st step ----------------------------------------------------------------
+sig = Y[,] %*% Y[,]
+selectTerms = NULL
+ERRvec = NULL
+gvec = NULL
+
+Qs = P
+g = rep(0,M)
+ERR = rep(0,M)
+for (m in 1:M){
+  g[m] = (Y[,] %*% Qs[,m]) / (Qs[,m] %*% Qs[,m])
+  ERR[m] = ( g[m]^2 * (Qs[,m] %*% Qs[,m]) ) / sig
+}
+l1 = which(ERR==max(ERR))
+selectTerms = l1 # vector keeping all selected terms
+
+# init
+A = diag(1,M)
+Qs = matrix(P[,l1],ncol = 1)
+gvec = g[l1]
+ERRvec = ERR[l1]
+
+# s-th step --------------------------------------------------------------
+for (s in 2:M){
+  gm  = rep(0,M)
+  Qm  = matrix(0,NP,M)
+  ERR = rep(0,M)
+  
+  for (m in (1:M)[-selectTerms]) {
+    
+    sumQm = rep(0,NP)
+    for (r in 1:(s-1)){
+      sumQm = sumQm + ((P[,m] %*% Qs[,r]) /  (Qs[,r] %*% Qs[,r])) %*% Qs[,r]
+    }
+    Qm[,m] = P[,m] - sumQm
+    
+    gm[m] = (Y[,] %*% Qm[,m]) / (Qm[,m] %*% Qm[,m])
+      
+    ERR[m] = ( gm[m]^2 * (Qm[,m] %*% Qm[,m]) ) / sig
+    
+  }
+  
+  ls = which(ERR==max(ERR))
+  selectTerms = cbind(selectTerms,ls) # vector keeping all selected terms
+  
+  Qs = cbind(Qs,Qm[,ls])
+  gvec = rbind(gvec,gm[ls])
+  for (r in 1:(s-1)){
+    A[r,s] = (Qs[,r] %*% P[,ls]) / (Qs[,r] %*% Qs[,r])
+  }
+  ERRvec = rbind(ERRvec,ERR[ls])
+  
+  ESR = 1-sum(ERRvec)
+  
+  if (ESR <= rho){
+    M0 = s
+    break
+  }
+}
+
+A = A[1:s,1:s]
+
+th_OLS = solve(A,gvec)
+
+P = P[,selectTerms]
+
+print("Selected terms")
+print(colnames(P))
+print("Estimated parameters")
+print(th_OLS[,])
+print(ERRvec[,]*100)
 
