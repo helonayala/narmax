6f25184afc6dc932ca429be509b12f55fa4404a0
helonayala <helon@puc-rio.br>
2019-01-16 17:23:40 -0200

    examples for JSS paper

--- examples/ex01_billings.R
+++ examples/ex01_billings.R
@@ -1,46 +1,30 @@
 # example 3.7 from the textbook. Billings, S.A. 2013 (p. 87)
 library(narmax)
-clearWorkspace()
+library(ggplot2)
 
 # generate simulation data ------------------------------------------------
-N = 400
-u = rnorm(N, mean = 0, sd=1)
-e = rnorm(N, mean = 0, sd=0.04^2)
-InitE = e
-y = rep(0, length(u))
+N <- 400
+u <- rnorm(N, mean = 0, sd=1)
+e <- rnorm(N, mean = 0, sd=0.04^2)
+y <- rep(0, length(u))
 for (k in 3:N) {
-  y[k] = 0.5*y[k-1] +
-    u[k-2] +
-    0.1*(u[k-2]^2) +
-    0.5*e[k-1] +
-    0.1*u[k-1]*e[k-2] +
-    e[k]
+  y[k] <- 0.5 * y[k-1] + u[k-2] + 0.1 * (u[k-2]^2) + 0.5 * e[k-1] + 0.1 * u[k-1] * e[k-2] + e[k]
 }
-e = rep(0,N)
 
 # define model structure --------------------------------------------------
-rho_p = 1e-2
-rho_n = 1.9e-6
-
-nu = 2
-ny = 2
-ne = 2
-nl = 2
-
-mdl = narmax(ny, nu, ne, nl)
+mdl <- narmax(ny = 2, nu = 2, ne = 2, nl = 2)
+mdl
 
 # estimate the model ------------------------------------------------------
-mdl = estimate(mdl, y, u, rho_p, rho_n)
-print(mdl)
+mdl <- estimate(mdl, y, u, rho_p = 1e-2, rho_n = 1.9e-6)
+mdl
 
 # calculate predictions ---------------------------------------------------
-P0 = predict(mdl, y, u, K = 0)
-P1 = predict(mdl, y, u, K = 1)
+P0 <- predict(mdl, y, u, K = 0)
+P1 <- predict(mdl, y, u, K = 1)
 
 # plot predictions/residuals ----------------------------------------------
-print(P0$ploty)
-print(P0$plote)
 print(P1$xcorrel) # validate with correlation-based tests
-
+ggsave("D:/Google Drive/Trabalho/Publicacoes/2018 JSS - narmax toolbox/paper/fig/ex1xcorrel.png", width=9, height=3)
 
 
--- examples/ex02_heatx.R
+++ examples/ex02_heatx.R
@@ -1,41 +1,30 @@
 # example 02 - data ftp://ftp.esat.kuleuven.be/pub/SISTA/data/thermic/heating_system.txt
 library(narmax)
-clearWorkspace()
 
 # read input/output data --------------------------------------------------
-data = matrix(scan("../data/heating_system.dat"),
+data <- matrix(scan("../data/heating_system.dat"),
                nrow=801,
                byrow=TRUE)
-N = nrow(data) # number of samples
-u = data[,2]
-y = data[,3]
-ie = 1:400
-iv = 401:801
-ye = y[ie]
-ue = u[ie]
-yv = y[iv]
-uv = u[iv]
+N <- nrow(data) # number of samples
+u <- data[,2]
+y <- data[,3]
+ie <- 1:400
+ye <- y[ie]
+ue <- u[ie]
 
 # model parameters
-na = 4
-nb = 4
-nc = 10
-mdl = armax(na,nb,nc)
+mdl <- armax(ny= 4,nu = 4,ne = 10)
+mdl
 
 # estimate parameters -----------------------------------------------------
-mdl = estimate(mdl,ye,ue)
+mdl <- estimate(mdl,ye,ue)
 mdl
 
 # calculate predictions ---------------------------------------------------
-# estimation phase
-Pe0 = predict(mdl, ye, ue, K = 0) # free-run
-Pe1 = predict(mdl, ye, ue, K = 1) # one-step-ahead
-# validation phase
-Pv0 = predict(mdl, yv, uv, K = 0) # free-run
-Pv1 = predict(mdl, yv, uv, K = 1) # one-step-ahead
 # all phases
-Pa0 = predict(mdl, y, u, K = 0) # free-run
-Pa1 = predict(mdl, y, u, K = 1) # one-step-ahead
+Pa0 <- predict(mdl, y, u, K = 0) # free-run
+Pe1 <- predict(mdl, ye, ue, K = 1) # one-step-ahead
+
 # print plots -------------------------------------------------------------
 print(Pa0$plote)   # plot residuals
 print(Pa0$ploty)   # plot predictions vs measured data
