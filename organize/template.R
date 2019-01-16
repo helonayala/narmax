3006320c81662136641bce896d4edafe9d5c45ea
helonayala <helon@puc-rio.br>
2019-01-16 17:22:53 -0200

    remove old code

--- organize/template.R
+++
@@ -1,9 +0,0 @@
-# narmax system identification
-# helon - 4/9/18
-# mec2015 - system identification - puc-rio
-
-rm(list=ls()) # remove all vars and functions
-cat("\014")   # clean console
-while(!is.null(dev.list())) dev.off()     # clear all graphs
-
-# code begins
